1b7d7d238097746afeb01f481b6c9dd2ae4612e1
Maheshwari2604 <maheshwarishivam2604@gmail.com>
2019-06-07 08:08:08 +0530

    Django_pro

--- hey/app1/admin.py
+++ hey/app1/admin.py
@@ -5,4 +5,16 @@ from django.contrib import admin
 
 # Register your models here.
 
-admin.site.register(reg)
+class register(admin.ModelAdmin):
+    list_display = ['id', 'lastname', 'username' ,'email', 'timestamp', 'updated', 'is_active']
+    list_editable = ['lastname', 'is_active']
+    search_fields = ['username', 'email']
+    date_hierarchy = 'timestamp'
+    readonly_fields = ['timestamp', 'updated']
+    prepopulated_fields = {"slug": ("username",)}
+
+    class meta:
+        model = reg
+
+admin.site.register(reg, register)
+
